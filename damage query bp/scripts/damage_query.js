de3b43e081b15c0f4dcde5e333acd93d90fe316d
Da4ThEdge <da4thedge@gmail.com>
2023-12-28 01:44:01 +0600

    Added a test particle with MolangVariableMap

--- damage query bp/scripts/damage_query.js
+++ damage query bp/scripts/damage_query.js
@@ -1,12 +1,21 @@
-import { world } from "@minecraft/server";
+import { world, MolangVariableMap } from "@minecraft/server";
 
 world.afterEvents.entityHealthChanged.subscribe(
     (onHitEvent) => {
         if(onHitEvent.entity.typeId === "edge:training_target"){
             let oldHP = onHitEvent.oldValue;
             let newHP = onHitEvent.newValue;
+            let entityLocation = onHitEvent.entity.getHeadLocation();
+            entityLocation.y = entityLocation.y + 0.6;
+            const molang = new MolangVariableMap();
+            molang.setColorRGB("variable.color", {
+                red: Math.random(),
+                green: Math.random(),
+                blue: Math.random()
+            });
             onHitEvent.entity.runCommand(`say Damage = §c${oldHP-newHP}§f\nCurrent HP = §a${newHP}`);
             onHitEvent.entity.runCommand(`playanimation @s hurt`);
+            onHitEvent.entity.dimension.spawnParticle("minecraft:colored_flame_particle", entityLocation, molang);
         }
     }
 );
